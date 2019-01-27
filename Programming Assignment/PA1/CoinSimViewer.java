04ead9a9ad017c8165c092a782796ba0f378ca04
Zifan Wang <zifwang@Zifans-MacBook-Air.local>
2019-01-26 23:45:54 -0800

    Delete some unnecessary code

--- Programming Assignment/PA1/CoinSimViewer.java
+++ Programming Assignment/PA1/CoinSimViewer.java
@@ -10,8 +10,6 @@
  */
 
 import javax.swing.JFrame;
-import java.awt.*;
-import java.awt.event.*;
 import java.util.Scanner;
 
 public class CoinSimViewer
@@ -23,9 +21,6 @@ public class CoinSimViewer
          */
         Scanner sc = new Scanner(System.in);                        // User input 
         int i = 0;                                                  // number of trails 
-        // CoinTossSimulator coinToss = new CoinTossSimulator();       // Coin toss simulator
-        boolean isLastTrail = false;                                // The boolean use to detect whether is the last trail.
-        String moreTrails;                                          // yes or no
 
         // Promote user input
         System.out.print("Enter number of trails: ");
@@ -35,51 +30,6 @@ public class CoinSimViewer
                     System.out.print("Enter number of trails: ");
                     i = sc.nextInt();
         }
-        // while(!isLastTrail){
-        //     System.out.print("Enter number of trails: ");
-        //     i = sc.nextInt();
-            
-        //     // Case to check whether input is correct
-        //     while(i <= 0){
-        //         System.out.println("ERROR: Number entered must be greater than 0.");
-        //         System.out.print("Enter number of trails: ");
-        //         i = sc.nextInt();
-        //     }
-            
-        //     // coinToss.run(i);
-        //     // display_helper(coinToss);
-
-        //     sc.nextLine();
-
-        //     // Promote more trails 
-        //     System.out.print("Do you want to add more trails [yes or no]: ");
-        //     moreTrails = sc.next();
-        //     // boolean isYesNo = false;
-        //     // if(moreTrails.equals("yes")){
-        //     //     isYesNo = true;
-        //     // }
-        //     // if(moreTrails.equals("yes")){
-        //     //     isYesNo = true;
-        //     // }
-        //     // while(!isYesNo){
-        //     //     System.out.println("ERROR: please enter yes or no.");
-        //     //     System.out.print("Do you want to add more trails [yes or no]: ");
-        //     //     moreTrails = sc.next();
-        //     //     if(moreTrails.equals("yes")){
-        //     //         isYesNo = true;
-        //     //     }
-        //     //     if(moreTrails.equals("yes")){
-        //     //         isYesNo = true;
-        //     //     }
-        //     // }
-        //     if(moreTrails.equals("yes")){
-        //         isLastTrail = false;
-        //     }
-        //     else{
-        //         isLastTrail = true;
-        //     }
-        // }
-
 
         // Summary graph display
         JFrame frame = new JFrame();     // Create a new window to hold graph
